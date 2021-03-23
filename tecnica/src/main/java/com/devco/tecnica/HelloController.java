fa035f63aec6740259271545ed63a9dd9123b8b6
Johan Camilo Diaz Diaz <johan.diaz@correo.icesi.edu.co>
2021-03-22 22:31:36 -0500

    feat(tecnica): Jenkinsfile actualizado

--- tecnica/src/main/java/com/devco/tecnica/HelloController.java
+++ tecnica/src/main/java/com/devco/tecnica/HelloController.java
@@ -7,12 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
     @RequestMapping(value = "/hello/{name}",
             method = RequestMethod.GET)
     public String getHello(
             @PathVariable("name") String name
-    ){
-        return "Hola "+name;
+    ) {
+        return "Hola " + name;
     }
 }
